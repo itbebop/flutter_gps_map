d746c5839c3879f9b75235e8a83f7d4148883905
icicle_4 <dwayne.han4@gmail.com>
2024-04-10 00:16:03 +0900

    feat: 지도 불러오기, api key hiding

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
@@ -15,19 +15,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: MapSample(),
+      home: GpsMapApp(),
     );
   }
 }
 
-class MapSample extends StatefulWidget {
-  const MapSample({super.key});
+class GpsMapApp extends StatefulWidget {
+  const GpsMapApp({super.key});
 
   @override
-  State<MapSample> createState() => MapSampleState();
+  State<GpsMapApp> createState() => GpsMapAppState();
 }
 
-class MapSampleState extends State<MapSample> {
+class GpsMapAppState extends State<GpsMapApp> {
   final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
@@ -35,14 +35,20 @@ class MapSampleState extends State<MapSample> {
     zoom: 14.4746,
   );
 
-  static const CameraPosition _kLake = CameraPosition(bearing: 192.8334901395799, target: LatLng(37.43296265331129, -122.08832357078792), tilt: 59.440717697143555, zoom: 19.151926040649414);
+  static const CameraPosition _kLake = CameraPosition(
+    bearing: 192.8334901395799,
+    target: LatLng(37.43296265331129, -122.08832357078792),
+    tilt: 59.440717697143555,
+    zoom: 19.151926040649414,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        //mapType: MapType.hybrid,
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex, // 처음 시작지점
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
